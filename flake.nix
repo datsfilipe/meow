bec8e6f2d2f01f951c9802298ca4dc6324d8530d
Filipe Lima <datsfilipe.foss@proton.me>
2025-12-07 21:05:21 -0300

    feat: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,57 @@
 {
-  description = "Meow!";
+  description = "A high-performance cat/bat alternative using your existing Neovim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-  in {
-    packages.x86_64-linux.meow = nixpkgs.legacyPackages.${system}.rustPlatform.buildRustPackage {
-      version = "1.0.1";
-      name = "meow";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
 
-      src = ./.;
-      cargoLock.lockFile = ./Cargo.lock;
-    };
+          src = ./.;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.meow;
-  };
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          buildInputs = [ ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          postInstall = ''
+            wrapProgram $out/bin/meow \
+              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.neovim pkgs.less ]}
+          '';
+
+          meta = with pkgs.lib; {
+            description = manifest.description;
+            homepage = "https://github.com/datsfilipe/meow";
+            license = licenses.mit;
+            mainProgram = "meow";
+          };
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+            hyperfine
+          ];
+        };
+      }
+    );
 }
